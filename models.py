14d3e1d9355793efc862ee9f78a2d31e71df330e
Huvinesh Rajendran <huvinesh@teleme.co>
2024-02-21 16:43:39 +0800

    changed dataclass to namedtuple

--- models.py
+++ models.py
@@ -1,15 +1,13 @@
 import sqlite3
-from dataclasses import dataclass
+from typing import NamedTuple
 
-@dataclass
-class Admin:
+class Admin(NamedTuple):
     id: int
     username: str
     password: str
 
 
-@dataclass
-class Conversation:
+class Conversation(NamedTuple):
     id: int
     created_at:str
     updated_at:str
@@ -20,29 +18,6 @@ class Conversation:
     message_feedback: str
     time_taken: float
 
-    def __getitem__(self, key: str):
-        if key == 'id':
-            return self.id
-        elif key == 'created_at':
-            return self.created_at
-        elif key == 'updated_at':
-            return self.updated_at
-        elif key == 'session_id':
-            return self.session_id
-        elif key == 'user_prompt':
-            return self.user_prompt
-        elif key == 'llm_response':
-            return self.llm_response
-        elif key == 'ratings':
-            return self.ratings
-        elif key == 'message_feedback':
-            return self.message_feedback
-        elif key == 'time_taken':
-            return self.time_taken
-        else:
-            raise KeyError(f'Invalid key: {key}')
-
-
 def create_user_input(user_prompt:str , session_id:str):
     conn = sqlite3.connect("telemechatbot.db")
     cursor = conn.cursor()
@@ -50,7 +25,6 @@ def create_user_input(user_prompt:str , session_id:str):
     conn.commit()
     cursor.close()
     conn.close()
-
 
 def update_conversation(llm_response:str, session_id:str):
     print(session_id)
@@ -61,7 +35,7 @@ def update_conversation(llm_response:str, session_id:str):
     latest_record : Conversation = cursor.fetchone()
     print(latest_record)
     if latest_record:
-        id = latest_record['id']
+        id = latest_record.id
         print(id)
         cursor.execute("UPDATE conversations SET llm_response = ? WHERE id = ?", (llm_response, id, ))
         conn.commit()
