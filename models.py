3890d16bd439c2301bb6e86172856f29312a7730
Huvinesh Rajendran <huvinesh@teleme.co>
2024-02-21 16:34:07 +0800

    added getitem for the dataclasses

--- models.py
+++ models.py
@@ -20,6 +20,28 @@ class Conversation:
     message_feedback: str
     time_taken: float
 
+    def __getitem__(self, key: str):
+        if key == 'id':
+            return self.id
+        elif key == 'created_at':
+            return self.created_at
+        elif key == 'updated_at':
+            return self.updated_at
+        elif key == 'session_id':
+            return self.session_id
+        elif key == 'user_prompt':
+            return self.user_prompt
+        elif key == 'llm_response':
+            return self.llm_response
+        elif key == 'ratings':
+            return self.ratings
+        elif key == 'message_feedback':
+            return self.message_feedback
+        elif key == 'time_taken':
+            return self.time_taken
+        else:
+            raise KeyError(f'Invalid key: {key}')
+
 
 def create_user_input(user_prompt:str , session_id:str):
     conn = sqlite3.connect("telemechatbot.db")
@@ -39,7 +61,7 @@ def update_conversation(llm_response:str, session_id:str):
     latest_record : Conversation = cursor.fetchone()
     print(latest_record)
     if latest_record:
-        id = latest_record.id
+        id = latest_record['id']
         print(id)
         cursor.execute("UPDATE conversations SET llm_response = ? WHERE id = ?", (llm_response, id, ))
         conn.commit()
